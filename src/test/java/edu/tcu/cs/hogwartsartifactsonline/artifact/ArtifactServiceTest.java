44bb2b3cccadb8ec4664f89eb1f0d311aac62faa
Ethan_nguyen <phuthanh0301@gmail.com>
2025-03-22 23:06:52 -0500

    fix

--- src/test/java/edu/tcu/cs/hogwartsartifactsonline/artifact/ArtifactServiceTest.java
+++ src/test/java/edu/tcu/cs/hogwartsartifactsonline/artifact/ArtifactServiceTest.java
@@ -4,7 +4,6 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-
 public class ArtifactServiceTest {
     @BeforeEach
     void setUp(){
