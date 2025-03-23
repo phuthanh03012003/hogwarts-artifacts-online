5e43ca9b3ed773f194609fb407894a895bb798a0
Ethan_nguyen <phuthanh0301@gmail.com>
2025-03-22 23:00:25 -0500

    ep 20

---
+++ src/test/java/edu/tcu/cs/hogwartsartifactsonline/artifact/ArtifactServiceTest.java
@@ -0,0 +1,23 @@
+package edu.tcu.cs.hogwartsartifactsonline.artifact;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+
+public class ArtifactServiceTest {
+    @BeforeEach
+    void setUp(){
+
+    }
+
+    @AfterEach
+    void tearDown(){
+
+    }
+
+    @Test
+    void findById(){
+
+    }
+}
