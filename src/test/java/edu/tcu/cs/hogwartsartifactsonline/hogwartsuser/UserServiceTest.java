d2fc8d9f2f95f6d4d9e035bce3ad40aa2c79da61
Ethan_nguyen <phuthanh0301@gmail.com>
2025-03-29 19:32:31 -0500

    User authentication and authorization done!

--- src/test/java/edu/tcu/cs/hogwartsartifactsonline/hogwartsuser/UserServiceTest.java
+++ src/test/java/edu/tcu/cs/hogwartsartifactsonline/hogwartsuser/UserServiceTest.java
@@ -9,11 +9,11 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
-
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.catchThrowable;
@@ -26,6 +26,9 @@ class UserServiceTest {
 
     @Mock
     UserRepository userRepository;
+
+    @Mock
+    PasswordEncoder passwordEncoder;
 
     @InjectMocks
     UserService userService;
@@ -131,6 +134,7 @@ class UserServiceTest {
         newUser.setEnabled(true);
         newUser.setRoles("user");
 
+        given(this.passwordEncoder.encode(newUser.getPassword())).willReturn("Encoded Password");
         given(this.userRepository.save(newUser)).willReturn(newUser);
 
         // When
