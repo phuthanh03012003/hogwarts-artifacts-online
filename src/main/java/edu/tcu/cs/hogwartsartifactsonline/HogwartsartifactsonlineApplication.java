3023ebc86e29658088e33583dc13af6ea0d85d72
Ethan_nguyen <phuthanh0301@gmail.com>
2025-04-07 15:20:23 -0500

    fix bug

--- src/main/java/edu/tcu/cs/hogwartsartifactsonline/HogwartsartifactsonlineApplication.java
+++ src/main/java/edu/tcu/cs/hogwartsartifactsonline/HogwartsartifactsonlineApplication.java
@@ -6,10 +6,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
-public class HogwartsArtifactsOnlineApplication {
+public class HogwartsartifactsonlineApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(HogwartsArtifactsOnlineApplication.class, args);
+        SpringApplication.run(HogwartsartifactsonlineApplication.class, args);
     }
 
     @Bean
